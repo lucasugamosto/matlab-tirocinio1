35ac36d46ba7d41c2a51823aa6a76d8a7bdfb87e
lucasugamosto <63077116+lucasugamosto@users.noreply.github.com>
2020-04-03 23:42:46 +0200

    Add files via upload

--- AndamentoJ.m
+++ AndamentoJ.m
@@ -1,4 +1,5 @@
 function AndamentoJ(A,B,v_max,xf)
+    %ANDAMENTO DELL'INDICE DI COSTO
     %per mostrare l'andamento dell'indice di costo in funzione della
     %variabile temporale v crea un grafico con sull'asse delle ascisse
     %la variabile v = 2,...,v_max, e sull'asse delle ordinate i valori
@@ -11,9 +12,8 @@ function AndamentoJ(A,B,v_max,xf)
     %contenente i valori di J al variare di v
     J = CalcolaIndiciDiCosto(A,B,v_max,xf);
     
-    plot(v,J,"r--*","LineWidth",2);
+    plot(v,J,"r-*","LineWidth",2);
     title("andamento di J");
     xlabel(" tempo discreto v");
     ylabel("indice di costo J");
-    
 end
